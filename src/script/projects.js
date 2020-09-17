1e831785bec6585d80c7acbd879e27a2a7440ff1
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-09-17 12:28:37 -0700

    compiled files

---
+++ src/script/projects.js
@@ -0,0 +1,10 @@
+const projects = document.getElementById("projects");
+
+const lang = {
+  "Node.JS / Express.JS": [],
+  "React.JS": [],
+  TypeScript: [],
+  "JavaScript (ES6+)": [],
+  SQL: [],
+  "HTML5 / CSS3": [],
+};
